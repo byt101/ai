50fee796ee91f80f4f2b5e3e9eb43ad447b7cb46
byt101 <bunnnyg09@gmail.com>
2025-05-06 01:10:56 +0530

    Update P3_Greedy_Search.py

--- P3_Greedy_Search.py
+++ P3_Greedy_Search.py
@@ -45,3 +45,33 @@ graph = {
 
 print("Prim's Algorithm")
 print("Prim's MST:", prim(graph, 'A'))
+
+
+# Kruskal's Algorithm
+
+def kruskal(graph):
+    edges = []
+    for u in graph:
+        for v in graph[u]:
+            edges.append((graph[u][v], u, v))  # (cost, from, to)
+    edges.sort()
+    parent = {node: node for node in graph}
+    def find(node):
+        if parent[node] != node:
+            parent[node] = find(parent[node])
+        return parent[node]
+    mst = []
+    for cost, u, v in edges:
+        root_u = find(u)
+        root_v = find(v)
+        if root_u != root_v:
+            parent[root_u] = root_v
+            mst.append((u, v, cost))
+    return mst
+graph = {
+    'A': {'B': 2, 'C': 3},
+    'B': {'A': 2, 'C': 1, 'D': 1},
+    'C': {'A': 3, 'B': 1, 'D': 4},
+    'D': {'B': 1, 'C': 4}
+}
+print("kruskal's MST: ",kruskal(graph))
