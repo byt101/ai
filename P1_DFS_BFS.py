e0037ecdf0155e7a6ba3084951c3f22b75139498
byt101 <bunnnyg09@gmail.com>
2025-05-05 16:30:03 +0530

    Update P1_DFS_BFS.py

--- P1_DFS_BFS.py
+++ P1_DFS_BFS.py
@@ -18,7 +18,7 @@ def dfs(graph, node, visited):
     if node not in visited:
         print(node, end=" ")
         visited.add(node)
-        for neighbor in graph[node]:
+        for neighbor in sorted(graph[node]):
             dfs(graph, neighbor, visited)
 
 # Breadth First Search (BFS)
